abdf7aa119be431d064d6367c85cb2f6f091da9e
Joan <joan.rivas.cepeda@gmail.com>
2024-10-03 04:37:49 -0300

    Added enemy
    need to fix enemy setting up and draw

--- main.lua
+++ main.lua
@@ -3,68 +3,156 @@
 function love.load()
     love.window.setTitle("MagiMagiSword")
     love.graphics.setBackgroundColor(1, 1, 1)
-    SwordImage = love.graphics.newImage("res/sword.png")
-    HeroImage = love.graphics.newImage("res/hero.png")
-    playerPosX = 800 /2 
-    playerPosY = 600/2
-    playerDamage = 2
-    playerHP = 10
-    playerCooldown = 0
-    isAttacking = false
-    playerSpeed = 50
-    playerAngle = 0
-    playerDirection = 1
+    math.randomseed( os.time() )
+
+    player = {
+    SwordImage = love.graphics.newImage("res/sword.png"),
+    HeroImage = love.graphics.newImage("res/hero.png"),
+    initPosX = 800/2,
+    initPosY = 600/2,
+    posX = 800/2,
+    posY = 600/2,
+    damage = 2,
+    hp = 10,
+    cooldown = 0,
+    isAttacking = false,
+    speed = 50,
+    angle = 0,
+    direction = 1,
     score = 0
+    }
+
+    enemy = {
+    posX = 0,
+    posY = 0,
+    hp = 0,
+    damage = 0,
+    angle = 0,
+    direction = 1,
+    speed = 25
+    }
+    enemyBody = love.graphics.newImage("res/enemy.png")
+    enemyEye = love.graphics.newImage("res/enemyEye.png")
 end
 
-function love.update(dt)
+function checkToSpawn(player)
+    local chancePerScore = player.score    
+    local randomNumb = 0
+
+    if (player.score > 10 ) then
+        chancePerScore = 10
+    end
+
+    randomNumb = math.random(20)
+
+    if (randomNumb <= chancePerScore) then
+        return true
+    else
+        return false
+    end
+end
+
+function setEnemy(player, enemy, enemyAlive)
+    local spawnLimit = 10
+    local currentSpawnLimit = player.score + 1
+
+    if (currentSpawnLimit > spawnLimit) then
+        currentSpawnLimit = spawnLimit
+    end
+
+    if (checkToSpawn(player) == true) then
+        if (currentSpawnLimit > enemyAlive) then
+            if (enemy.hp <= 0) then
+                enemy.posX = math.random(1, 200)
+                enemy.posY = math.random(200, 599)
+                enemy.hp = 2 * (player.score + 1)
+                enemy.damage = 1* (player.score + 1)
+            end
+        end
+    end
+
+    return enemy
+end
+
+function playerUpdateState(dt, player)
     if love.keyboard.isDown('right') then
-        playerPosX = playerPosX + playerSpeed * dt
-        playerDirection = 2
+        player.posX = player.posX + player.speed * dt
+        player.direction = -1
    end
    if love.keyboard.isDown('left') then
-       playerPosX = playerPosX - playerSpeed * dt
-       playerDirection = 1
+       player.posX = player.posX - player.speed * dt
+       player.direction = 1
    end
    if love.keyboard.isDown('down') then
-       playerPosY = playerPosY + playerSpeed * dt
+       player.posY = player.posY + player.speed * dt
    end
    if love.keyboard.isDown('up') then
-       playerPosY = playerPosY - playerSpeed * dt
+       player.posY = player.posY - player.speed * dt
    end
    if love.keyboard.isDown('z') then
-        if (playerCooldown == 0) then
-        isAttacking = true
-        playerCooldown = 60
+        if (player.cooldown == 0) then
+        player.isAttacking = true
+        player.cooldown = 60
         end
    end
 
-   if (playerCooldown > 0) then
-   playerCooldown = playerCooldown - 1
-   end
-   if (playerCooldown == 0 ) then
-    isAttacking = false
-   end
+   if (player.cooldown > 0) then
+    player.cooldown = player.cooldown - 1
+    end
+    if (player.cooldown == 0 ) then
+     player.isAttacking = false
+    end
+
+
+   return player
+end
+
+function drawPlayer(player)
+    if (player.direction == 1) then
+        if (player.isAttacking == false) then
+             love.graphics.draw(player.SwordImage, (player.posX-70), player.posY - 10, player.angle, player.direction, 1)
+        else
+            love.graphics.draw(player.SwordImage, (player.posX-100), player.posY + 120, math.rad(270), player.direction, 1)
+        end
+    love.graphics.draw(player.HeroImage, (player.posX-50), player.posY, player.angle, player.direction, 1)
+    else
+        love.graphics.draw(player.HeroImage, (player.posX+50), player.posY, player.angle, player.direction, 1)
+        if (player.isAttacking == false) then
+        love.graphics.draw(player.SwordImage, (player.posX+70), player.posY - 10, player.angle, player.direction, 1)
+        else
+            love.graphics.draw(player.SwordImage, (player.posX+100), (player.posY + 120), math.rad(90), player.direction, 1)
+        end
+    end
+end
+
+
+
+function love.update(dt)
+    
+    local enemyAlive = 0
+    player = playerUpdateState(dt, player)
+
+   for i = 0, 9 do
+        if  (enemy[i].hp > 0) then
+            enemyAlive = enemyAlive + 1
+        end
+    end
+    for y = 0, 9 do 
+        setEnemy(player, enemy[y], enemyAlive)
+    end
   
 
 end
 
 function love.draw()
-    love.graphics.setColor(1, 1, 1)
 
-    if (playerDirection == 1) then
-        if (isAttacking == false) then
-             love.graphics.draw(SwordImage, (playerPosX-70), playerPosY - 10, playerAngle, 1, 1)
-        else
-            love.graphics.draw(SwordImage, (playerPosX-100), playerPosY + 120, math.rad(270), 1, 1)
-        end
-    love.graphics.draw(HeroImage, (playerPosX-50), playerPosY, playerAngle, 1, 1)
-    else
-        love.graphics.draw(HeroImage, (playerPosX+50), playerPosY, playerAngle, -1, 1)
-        if (isAttacking == false) then
-        love.graphics.draw(SwordImage, (playerPosX+70), playerPosY - 10, playerAngle, -1, 1)
-        else
-            love.graphics.draw(SwordImage, (playerPosX+100), playerPosY + 120, math.rad(90), -1, 1)
+    love.graphics.setColor(1, 1, 1)
+    drawPlayer(player)
+
+    for i = 0, 9 do
+        if  (enemy[i].hp > 0) then
+            love.graphics.draw(enemyBody, (enemy[i].posX), (enemy[i].posY), enemy[i].angle, enemy[i].direction, 1)
         end
     end
-  end
+
+end
